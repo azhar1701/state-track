13084161e95af17a900c3131c76924a55f6c74c8
Ari Azhar Maulana <ti3.ari170197@gmail.com>
2026-01-07 06:38:37 +0700

    Set Vite base path to root for deployment
    Changed the base path in vite.config.ts to always use '/', ensuring compatibility with root deployments on Vercel.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const base = mode === "production" ? "/state-track/" : "/";
+  // Untuk deployment di root Vercel, gunakan base: '/'.
+  const base = "/";
 
   return {
     server: {
